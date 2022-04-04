8e2314b853b9cbaa313db0f12237fa8959726740
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-04 21:30:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Make a greeting of the program
+print("Welcome fellow musician, I see you seek for a new name of your band...\nBut first I will need some information from you!")
+# Ask user for the name of the city the grew up in.
+print("What is the name of the city you grew up in?")
+city = input()
+# Ask the user for the name of pet.
+print("What is the name of your pet?")
+pet = input()
+# Combine name of city and pet to display a band name.
+print("Your new band name is... \n" + (city + pet))
+# Make sure the input cursor displays on a new line.
